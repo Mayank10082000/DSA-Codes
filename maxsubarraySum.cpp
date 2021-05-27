0be9224c1ce836b69ef6834fa5d3d5ce935706b1
anirudh09041 <anirudh09041@gmail.com>
2021-05-27 10:43:05 +0530

    comment added in maxsubarraySum

--- maxsubarraySum.cpp
+++ maxsubarraySum.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 int main()
 {
+//finds max sub array sum
+	int array[8] = {−1, 2, 4, −3, 5, 2, −5, 2};
+	int best = 0;
+	for (int a = 0; a < 8; a++) {
+		int sum = 0;
+		for (int b = a; b < 8; b++) {
+			sum += array[b];
+			best = max(best,sum);
+		}
 
-int array[8] = {−1, 2, 4, −3, 5, 2, −5, 2};
-int best = 0;
-for (int a = 0; a < 8; a++) {
-int sum = 0;
-for (int b = a; b < 8; b++) {
-sum += array[b];
-best = max(best,sum);
-}
-
-}
-cout << best << "\n";
+	}
+	cout << best << "\n";
 
 }
 
